629ec554a03958ef56e35c5b5fe2b0b67e33fea7
bchoi12 <brian.choi222@gmail.com>
2022-06-01 22:22:11 -0700

    ok put the turn server in the right spot

--- client/src/connection.js
+++ client/src/connection.js
@@ -6,11 +6,23 @@ class Connection {
         this._iceConfig = {
             "iceServers": [
                 {
-                    urls: [
-                        "stun:stun1.l.google.com:19302",
-                        "stun:stun2.l.google.com:19302",
-                    ]
-                }
+                    urls: "stun:openrelay.metered.ca:80",
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:80",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                },
             ]
         };
         this._handlers = new Map();
--- client/ts/connection.ts
+++ client/ts/connection.ts
@@ -7,12 +7,24 @@ type MessageSender = () => void;
 class Connection {
 	private readonly _iceConfig = {
     	"iceServers": [
-	    	{
-	    		urls: [
-	                "stun:stun1.l.google.com:19302",
-	                "stun:stun2.l.google.com:19302",
-	    		]
-	    	}
+			{
+		      urls: "stun:openrelay.metered.ca:80",
+		    },
+		    {
+		      urls: "turn:openrelay.metered.ca:80",
+		      username: "openrelayproject",
+		      credential: "openrelayproject",
+		    },
+		    {
+		      urls: "turn:openrelay.metered.ca:443",
+		      username: "openrelayproject",
+		      credential: "openrelayproject",
+		    },
+		    {
+		      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+		      username: "openrelayproject",
+		      credential: "openrelayproject",
+		    },
 	    ]
     };
 
