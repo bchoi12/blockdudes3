4c68f1caade5772f6d95e3895881c166607a2c11
bchoi12 <brian.choi222@gmail.com>
2022-07-01 22:21:20 -0700

    remove autogenerated javascript src files

--- client/src/particles.js
+++
@@ -1,6 +0,0 @@
-import { SceneComponent } from './scene_component.js';
-export class Particles extends SceneComponent {
-    constructor() {
-        super();
-    }
-}
--- client/src/player.js
+++
@@ -1,6 +0,0 @@
-class RenderObject extends THREE.Mesh {
-    constructor() {
-        super();
-        debug(this);
-    }
-}
